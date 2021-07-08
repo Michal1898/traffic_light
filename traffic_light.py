873aed70c554942b22157b03a4c60c3a5bd850a5
Michal1898 <michal.volf@gmail.com>
2021-07-08 08:10:05 +0200

    random choice, don´t repeat twice in row.

--- traffic_light.py
+++ traffic_light.py
@@ -1,28 +1,37 @@
 from random import choice
 
-def random_message():
-    random_warning_list=["Tato barva není na semaforu." , "Asi porucha", "Vyber správnou barvu!",
-                         "Červená, žlutá, nebo zelená", "Ty jsi případ!" , "Lorem ipsum dolor"]
-    message = choice(random_warning_list)
-    return message
+traffic_light={"červená" :"Zůstaň stát!" , "žlutá" :"Připrav se!" , "zelená" : "Jeď"}
+random_warning_list = ["Tato barva není na semaforu.", "Asi porucha", "Vyber správnou barvu!",
+                       "Červená, žlutá, nebo zelená", "Ty jsi případ!", "Lorem ipsum dolor"]
 
-def return_answer (shining_light):
-    traffic_light={"červená" :"Zůstaň stát!" , "žlutá" :"Připrav se!" , "zelená" : "Jeď"}
-    what_to_do =  traffic_light.get(shining_light)
-    if what_to_do == None:
-        random_warning = random_message()
-        return random_warning
-    else:
-        return what_to_do
+def last_warning_message(msg_text):
+    return msg_text
+
+
+def random_message(last_warning_text):
+    temp_warning_list = random_warning_list.copy()
+    if last_warning_text in temp_warning_list:
+        temp_warning_list.remove(last_warning_text)
+    message = choice(temp_warning_list)
+    return message
 
 def main():
     repeat_cycle = True
+    last_warning_msg=""
     while repeat_cycle :
         repeat_cycle = False
+        print(f"Poslední náhodný vzkaz: {last_warning_msg}")
 
-        traffic_light = input("Zadej barvu světla na semaforu: ")
-        your_behaviour = return_answer(traffic_light)
-        print(your_behaviour)
+        color_of_light = input("Zadej barvu světla na semaforu: ")
+        what_to_do = traffic_light.get(color_of_light)
+        if what_to_do == None:
+            random_warning = random_message(last_warning_msg)
+            last_warning_msg = random_warning
+            print (random_warning)
+        else:
+            last_warning_msg = ""
+            print (what_to_do)
+
         once_again = input("Opakovat cyklus ? (Ano/Ne) ")
         once_again = once_again.lower()
         if once_again == "ano" :
